d654930a82d012c64bbd9fa259c2d9c773238435
coderRaj07 <12131501.edu@gmail.com>
2023-08-03 13:54:41 +0530

    FEAT_3: builder object issue warning resolved

--- src/features/cartSlice.js
+++ src/features/cartSlice.js
@@ -1,45 +1,46 @@
-import { createAsyncThunk,createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from 'axios';
 
-export const getAllData = createAsyncThunk("getProducts",async()=>{
- const res = await axios.get('https://fakestoreapi.com/products')
- const result = res.data;
- return result;
+export const getAllData = createAsyncThunk("getProducts", async () => {
+  const res = await axios.get('https://fakestoreapi.com/products')
+  const result = res.data;
+  return result;
 })
 
-const initialState = { 
-    cart: [],
-    items: [],
-    totalQuantity: 0,
-    totalPrice: 0,
+const initialState = {
+  cart: [],
+  items: [],
+  totalQuantity: 0,
+  totalPrice: 0,
 };
 
 export const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    addToCart: (state,action)=>{
+    addToCart: (state, action) => {
       state.cart.push(action.payload)
     }
   },
-  extraReducers:{
-    [getAllData.pending]:(state)=>{
-      state.totalQuantity = 0;
-      state.totalPrice = 0
-    },
-    [getAllData.fulfilled]:(state,action)=>{
-      state.items = action.payload;
-      state.totalQuantity = state.items.length;
-      state.totalPrice = 0
-    },
-    [getAllData.rejected]:(state,action)=>{
-      state.items = []
-      state.totalQuantity = 0;
-      state.totalPrice = 0
-    }
-  }
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllData.pending, (state) => {
+        state.totalQuantity = 0;
+        state.totalPrice = 0;
+      })
+      .addCase(getAllData.fulfilled, (state, action) => {
+        state.items = action.payload;
+        state.totalQuantity = state.items.length;
+        state.totalPrice = 0;
+      })
+      .addCase(getAllData.rejected, (state, action) => {
+        state.items = []
+        state.totalQuantity = 0;
+        state.totalPrice = 0;
+      });
+  },
 });
 
-export const {addToCart} = cartSlice.actions;
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
