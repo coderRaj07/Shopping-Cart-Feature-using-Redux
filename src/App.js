8e6abbb914593bdb13283d2fea4bf857e4da3b74
coderRaj07 <12131501.edu@gmail.com>
2023-08-03 14:46:55 +0530

    FEAT_3: BrowserRouter,Routes,Route added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import ProductCard from './components/ProductCard'
-import {useDispatch} from 'react-redux';
-import {getAllData} from './features/cartSlice';
+import CartPage from './components/cartPage'
+import { useDispatch } from 'react-redux';
+import { getAllData } from './features/cartSlice';
 import React, { useEffect } from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
 
   const dispatch = useDispatch();
-
   // Dispatch the action when the component mounts (page loads)
   // So that we can display data in subsequent components using useSelector()
   useEffect(() => {
     dispatch(getAllData());
-  }, [dispatch]); 
+  }, [dispatch]);
 
   return (
+    <BrowserRouter>
       <div>
-        <Navbar/>
-        <ProductCard/>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<ProductCard/>}/>
+          <Route path="/cart" element={<CartPage/>}/>
+        </Routes>
       </div>
+    </BrowserRouter>
   )
 }
 
